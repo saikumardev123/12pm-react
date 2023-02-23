c9078b8956e16608089df64194aae01bf627e902
sai <sai@digital-lync.com>
2023-02-23 12:59:37 +0530

    23feb2023

---
+++ 23Feb2023/index.js
@@ -0,0 +1,25 @@
+
+
+var employee = {
+    name: "Kiran",
+    Rep_manager: "Rajesh",
+    hike: "10%",
+    currentCTC: "10LPA",
+    revisedCTC: "11LPA",
+    role: "Senior Dev"
+}
+
+
+var str2 = `
+                Dear ${employee.name}, 
+                 
+                                          Based on your performance, contribution to the organization. You have got the 
+                                          highest hike of ${employee.hike}. Your current ctc is ${employee.currentCTC} is revised to 
+                                          ${employee.revisedCTC}.  
+
+                                          Your new role is ${employee.role}. 
+
+                 Thanks, 
+                  ${employee.Rep_manager}
+             `;
+console.log(str2);
