30ae50a3be77cf6b977845d050ecc2b607f7d79e
sai <sai@digital-lync.com>
2023-03-15 12:30:12 +0530

    15 march

--- 15March2023/youtubeclone/src/App.js
+++ 15March2023/youtubeclone/src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 const API_KEY = "AIzaSyCUcjp6sD1wa_8chtI4 - aLfdDEKgfg3w_Y";
 function App() {
   useEffect(
-    
     () => {
       console.log("use effect called")
       var API_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&q=java&type=video&maxResults=5`;
       axios.get(API_URL).then(
-        responsae => {
-          console.log(response);
+        response => {
+          console.log(response.data);
         },
         error => {
           console.log(error);
@@ -31,5 +31,22 @@ export default App;
 /*
 
 https://www.googleapis.com/youtube/v3/search?part=snippet&key=&q=java&type=video&maxResults=5
+
+*/
+
+
+/*
+
+Steps: 
+
+1. maintain the list of videos in useState 
+
+2. create a video list component 
+
+3. send videolist from app component to videolist component and access it 
+through props.
+
+ {props.videos.length}
+
 
 */
